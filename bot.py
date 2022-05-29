5937961787f5e825cc314fe98b7cfaff419b69e4
Ramon Arambula <r_arambula@u.pacifc.edu>
2022-05-28 22:41:01 -0700

    Added ability to pick times from bot message

--- bot.py
+++ bot.py
@@ -25,6 +25,7 @@ async def on_ready():
     active_servers = client.guilds
     for guild in active_servers:
         print(guild.name)
+    print()
 
 @client.command()
 async def reserve(ctx):
@@ -47,7 +48,7 @@ async def reserve(ctx):
 @client.command()
 async def emoji(ctx):
     channel = ctx.channel
-    list_of_times = times_between_xy(ceil_dt(dt=datetime(NOW.year, NOW.month, NOW.day, hour=15, minute=30, second=0),
+    list_of_times = times_between_xy(ceil_dt(dt=datetime(NOW.year, NOW.month, NOW.day, hour=16, minute=00, second=0),
                             delta=timedelta(minutes=30)))
     str_of_times = [str(i).split(' ')[1] for i in list_of_times]
     str_of_times = [f"{str(i)} {list_of_times[i]}" for i in range(len(list_of_times))]
@@ -67,8 +68,6 @@ async def emoji(ctx):
     await message.add_reaction("\U0000274C") # X Emoji
     await message.add_reaction("\U00002705") # Check mark emoji
 
-    
-
 @client.event
 async def on_reaction_add(reaction, user):
     # TODO Add a check that only counts the emojis added by person who invoked -reserve
@@ -76,10 +75,20 @@ async def on_reaction_add(reaction, user):
     if user.bot == False:
         if reaction.emoji == '\U0000274C': # if X emoji
             await reaction.message.channel.send(f"Cancelled the reservation process!")
-            print(reaction.message.reactions)
             await reaction.message.clear_reactions()
         elif reaction.emoji == '\U00002705':
-            await reaction.message.channel.send(f"Making the reservation!")
+            await reaction.message.channel.send(f"Making the reservation!") #! add times selected to print
+            sent_message:str = (reaction.message.content).split('\n')[1:] 
+            print("TIMES")
+            for i in sent_message:
+                print(i)
+            times_selected = []
+            print()
+            for react in reaction.message.reactions:
+                if react.count > 1:
+                    for i in sent_message: 
+                        if react.emoji in i:
+                            times_selected.append(i.split(' ')[-1])
             await reaction.message.clear_reactions()
-        
+            print(times_selected)
 client.run(TOKEN)
