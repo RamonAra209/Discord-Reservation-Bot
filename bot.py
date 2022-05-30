82b91a1e87d7a9a5129247e51d16d1899c8fb438
Ramon Arambula <r_arambula@u.pacifc.edu>
2022-05-29 22:13:49 -0700

    Added -deleteme command, and renamed main command to reserve

--- bot.py
+++ bot.py
@@ -16,39 +16,17 @@ PATH_TO_RESERVE = "python3 /Users/tahpramen/Developer/Personal\ Projects/LRT_V2/
 
 intents = discord.Intents.default()
 intents.members = True
-# client = discord.Client(intents=intents)
 client = commands.Bot(command_prefix='-', intents=intents)
 
 @client.event
 async def on_ready():
-    print(f"{client.user} is connected to guilds")
+    print(f"{client.user} is connected to guilds:")
     active_servers = client.guilds
     for guild in active_servers:
-        print(guild.name)
-    print()
+        print("\t", guild.name)
 
 @client.command()
 async def reserve(ctx):
-    now = datetime.now()
-    nearest_thirty = ceil_dt(now, timedelta(minutes=30))
-
-    await ctx.send(f"{ctx.channel}: {ctx.author}")
-    output = subprocess.check_output(f"{PATH_TO_RESERVE} 12:00:00 14:00:00 {ctx.author}" , shell=True)
-    await ctx.send(f"Result from calling reservation: {str(output)}")
-    if check_output_from_reserve(output) == False:
-        await ctx.send(f"{ctx.author.name} you fuck, you're not in the database, add yo shit. (check your DMs!)")
-        user_channel = await ctx.author.create_dm()
-        await user_channel.send("How do you want me to reserve you a room if you're not in the database?")
-        await user_channel.send("Answer these next few questions so I can add you!")
-        await user_channel.send("TODO")
-    else:
-        times_to_print = times_between_xy(nearest_thirty)
-        str_of_times = [str(i).split(' ')[1] for i in times_to_print]
-
-@client.command()
-async def emoji(ctx):
-    print(ctx.author, type(ctx.author))
-
     users_json = None
     with open('users.json') as f:
         users_json = json.load(f)
@@ -68,7 +46,7 @@ async def emoji(ctx):
         for key, val in time_dict.items():
             str_to_print += f"{NUM_EMOJIS[int(key)]} {val}\n"
         
-        message = await ctx.send(f"↓↓↓{ctx.author.mention} React to two time slots, and press check mark when ready↓↓↓ \n{str_to_print}") 
+        message = await ctx.send(f"↓↓↓{ctx.author.mention} React to two time slots, and press check mark when ready, or X to cancel↓↓↓ \n{str_to_print}") 
         for key in time_dict.keys():
             await message.add_reaction(NUM_EMOJIS[int(key)])
         await message.add_reaction("\U0000274C") # X Emoji
@@ -104,6 +82,7 @@ async def emoji(ctx):
             # await asyncio.sleep(0.75)
         except asyncio.TimeoutError:
             await user_dm.send("You ran out of time to answer!")
+            return
 
         first = str(first.content)
         last = str(last.content)
@@ -118,14 +97,14 @@ async def emoji(ctx):
                         }
                     } 
         
-        data = None
+        database = None
         with open('users.json') as f:
-            data = json.load(f)
-        data.update(user_dict)
+            database = json.load(f)
+        database.update(user_dict)
         with open('users.json', 'w') as f:
-            json.dump(data, f, indent=4, separators=(", ", ": "), sort_keys=True)
-        
-        await user_dm.send("Successfully added you to the database. If at any point you messed up, call '-delete_me' in the discord server and repeat the steps. Or if you just don't want to be in the database, then that's okay too.")
+            json.dump(database, f, indent=4, separators=(", ", ": "), sort_keys=True)
+
+        await user_dm.send("Successfully added you to the database. If at any point you messed up, call '-deleteme' in the discord server and repeat the steps. Or if you just don't want to be in the database, then that's okay too. \n\nNow go back to the server and call '-reserve' again to complete the reservation!")
 
 @client.event
 async def on_reaction_add(reaction, user):
@@ -151,21 +130,21 @@ async def on_reaction_add(reaction, user):
             await reaction.message.channel.send(f"Making the reservation between {times_selected[0]} and {times_selected[-1]}! Check your email for confirmation!") #! add times selected to print
             print(times_selected)
 
-            print(user)
-            now = datetime.now()
-            nearest_thirty = ceil_dt(now, timedelta(minutes=30))
-
             output = subprocess.check_output(f"{PATH_TO_RESERVE} {times_selected[0]} {times_selected[-1]} {user}", shell=True)
-            # await reaction.channel.send(f"Result from calling reservation: {str(output)}")
-            # if check_output_from_reserve(output) == False:
-            #     await reaction.channel.send(f"{reaction.message.author.name} you fuck, you're not in the database, add yo shit. (check your DMs!)")
-            #     user_channel = await user.create_dm()
-            #     await user_channel.send("How do you want me to reserve you a room if you're not in the database?")
-            #     await user_channel.send("Answer these next few questions so I can add you!")
-            #     await user_channel.send("TODO")
-            # else:
-            #     times_to_print = times_between_xy(nearest_thirty)
-            #     str_of_times = [str(i).split(' ')[1] for i in times_to_print]
-            #! MAKE RESERVATION HERE WITH SUBPROCESS CALL
+            print(output)
+
+@client.command()
+async def deleteme(ctx):
+    database:dict = None
+    with open('users.json') as f:
+        database = json.load(f)
+
+    database.pop(str(ctx.author))
+    
+    with open('users.json', 'w') as f:
+        json.dump(database, f, indent=4, separators=(", ", ": "), sort_keys=True)
+
+    await ctx.send("Deleted you from database!")
+    
 
 client.run(TOKEN)
